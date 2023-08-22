a2a5c23a09477f3238f190ad618d081aaebee95a
Felix <felixrunye@gmail.com>
2023-08-22 17:28:47 +0300

    updated api routes

--- src/pages/api/edge-runtime-large-create/index.tsx
+++ src/pages/api/edge-runtime-large-create/index.tsx
@@ -3,7 +3,7 @@ export const runtime = 'experimental-edge'
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   try {
-    const response = await fetch('https://my-json-server.typicode.com/typicode/demo/posts');
+    const response = await fetch('https://dummyjson.com/products');
     const data = await response.json();
 
     res.status(200).json(data);
