ddc6a6d48c666a4b70e977f332d82f40b7c0d143
Felix <felixrunye@gmail.com>
2023-08-31 11:22:42 +0300

    updated edge function

--- src/pages/api/edge-runtime-small-create/index.tsx
+++ src/pages/api/edge-runtime-small-create/index.tsx
@@ -12,7 +12,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     const response = await fetch('https://my-json-server.typicode.com/typicode/demo/db');
     const data = await response.json();
 
-    NextResponse.json(data);
+    return NextResponse.json(data);
   } catch (error) {
     console.error('Error fetching data:', error);
     res.status(500).json({ message: 'Internal server error' });
